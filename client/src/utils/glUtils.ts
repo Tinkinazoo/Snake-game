11a4d0265fdcbf2178e1fa88f693eec87d3dfafc
Tinkinazoo <127057803+Tinkinazoo@users.noreply.github.com>
2025-06-05 16:34:52 +0300

    Create glUtils.ts

---
+++ client/src/utils/glUtils.ts
@@ -0,0 +1,10 @@
+export const initWebGL = (canvas: HTMLCanvasElement): WebGLRenderingContext | null => {
+  const gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+  
+  if (!gl) {
+    console.error('Unable to initialize WebGL. Your browser may not support it.');
+    return null;
+  }
+  
+  return gl;
+};
